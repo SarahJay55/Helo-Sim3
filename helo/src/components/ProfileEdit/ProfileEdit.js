421000bebf73340154a26bfa9b3ce0107765333e
eunbink <kangeb1992@gmail.com>
2017-10-06 15:52:54 -0600

    yay

--- helo/src/components/ProfileEdit/ProfileEdit.js
+++ helo/src/components/ProfileEdit/ProfileEdit.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './ProfileEdit.css';
 import axios from 'axios';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import Header from '../Header/Header'
 
 
 class ProfileEdit extends Component {
@@ -9,23 +10,223 @@ class ProfileEdit extends Component {
     render() {
         return (
             <div className="profilepage">
-           <div className = "profile_navbar">
-               <div className="helo">Helo
-                   <img className="homeimage" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAXCAYAAAAV1F8QAAAAAXNSR0IArs4c6QAAAZ9JREFUSA3tlL1KA0EUhXc1QlCwShNimcIu2giChV36YBB7C3vxCcwL6CMEgo3gE2hnH/zBKrGztBQEcf1OuBt2Bmcza+3A596595wzJrvZJKmwsixrw73RrmBNlmLFhB+jHcOKMbZebES5jrAG3MAXDKBmqFZPs0Z5yoIpAV14g1fY8+Xq2Uyarj9fuMdUhwv4hiGsh0yamUZaeeohrdNH2IEneIdDZ1iykdY88naCUoYpnMIn3MFGUBwYyGNeZSgrdaQ0WnALEpyBK3DU5Rt5LUNZymzNHBQ90Nf0DFvlMfFTZVmmsnsJfx7hEuJuYvxZytZDpewHx0bjBXad5h82ylBW0eq/GTYZVn4IioFWK0NZ81WbVxEF/+URsgOTXqdpehVhm0n8TxT0cciA4Qh0L8WI3jnX6gujVt930muC3msn+Uy19Zp5L7/S70OW73WN/UTbaJdhKJMt1epptnDFHrSmJO7JR55YqGezvB+6xh4U8kf3/w+K/qp84W8/2H2ezFVPuOPti9uq+kQvwYme/8CaFtNVo5sGtGpPivofLh2TqPQC754AAAAASUVORK5CYII="/>
-                    <img className="searchimage" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAAXCAYAAADgKtSgAAAAAXNSR0IArs4c6QAAAeJJREFUSA2tlM0rBGEcx3dWEREp4oBVrvsXLJEoTsR/QDk6ydt1E2ccuIniKGeEi4MLbY6SAzaRUt5C1uer9pkZM7PM2m99muf5vT0zz/yeJxLxUSaTKYch2IJzeIF7SMECdPikeUzWTwuJI9iSUA0HcAhpKIVm6IEWkG/UsqwUz9yiaAmswScsQ31QBr4uOIEnGAyKM3aCVFjB/caYY0CcXmYVPqA7MBTnCOiN/1Q4W4h4C/Rf9D9qs3bzxKifdwNLxhhiQF4V3MGiJw2juuIN6jzOPxrInYRnKHOlYNBnbbuMISfkx0Dqc6ZGmcRB7Za3aMcLkq9BtYxUXC2nPv6vVMPVvir+Ajog/5VqvDqLqPgVxJzGsGP2uoicBlAtIxXfh15jyW+QIK0CdCXYYtV2kDpta7gRuRtwBpYnE+MeHEOxx/mLgZxW0Oke9g3FEYdHWAHv6r5ZkQixTZCGXdAW+wvnALzDJlT6R9lWYhKgwtKM7QkYEaSrVJfQLYxDozOUeRTaYB20FTuQBGnKGes7JqgG5kHXr3QJR3AKDyDp5+lO+t4KnhMguRYI3F8CdSh0T+tI6+TpgKiP1W7HHPkMTyPiJ5jMwTS+WeMo1EALgOT6gkLVVwdlF9CXFF4sMAapL60vtT4ftHlFAAAAAElFTkSuQmCC"/>
-                   </div>
-            
-               <div className="profile">Profile</div>
-               <div className="logout">Logout</div>
-
-           </div>
+                <Header page="Profile"/>
+          
+           
            <div className="parentbox">
            <div className = "profilebox">
                <img className="profileimage"src="https://robohash.org/me"/>
+               Eunbin Kang
+               <button className="cancelbutton">Cancel</button>
                <button className="updatebutton">Update</button>
-               <button className="searchbutton">Search</button>
+               
+              
            </div>
-           <div className = "profile_info"></div>
+           <div className = "profile_info">
+                <div className="options">First Name
+                    <input className="firstname"/></div>
+                <div className="options">Last Name
+                    <input className="lastname"/></div>
+                
+                <div className="options">Gender
+                    <select className="options"> 
+                    <option>Female</option>
+                    <option>Male</option>
+                    </select></div>
+                <div className="options">Hair Color
+                <select className="options">
+                    <option>Brown</option>
+                    <option>Black</option>
+                    <option>Blonde</option>
+                    <option>Red</option>
+                    <option>White</option>
+                    <option>Blue</option>
+                    </select>
+                </div>
+                <div className="options">Eye Color
+                <select className="options">
+                    <option>Brown</option>
+                    <option>Blue</option>
+                    <option>Green</option>
+                    </select>
+                </div>
+                <div className="options">Hobby
+                <select className="options">
+                    <option>Video Games</option>
+                    <option>Hiking</option>
+                    <option>Fishing</option>
+                    <option>Rafting</option>
+             
+                    </select>
+                </div>
+                <div className="options">Birthday Day
+                <select className="options">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                    <option>13</option>
+                    <option>14</option>
+                    <option>15</option>
+                    <option>16</option>
+                    <option>17</option>
+                    <option>18</option>
+                    <option>19</option>
+                    <option>20</option>
+                    <option>21</option>
+                    <option>22</option>
+                    <option>23</option>
+                    <option>24</option>
+                    <option>25</option>
+                    <option>26</option>
+                    <option>27</option>
+                    <option>28</option>
+                    <option>29</option>
+                    <option>30</option>
+                    <option>31</option>
+
+                    </select>
+                </div>
+                <div className="options">Birthday Month
+                <select className="options">
+                    <option>January</option>
+                    <option>February</option>
+                    <option>March</option>
+                    <option>April</option>
+                    <option>May</option>
+                    <option>June</option>
+                    <option>July</option>
+                    <option>August</option>
+                    <option>September</option>
+                    <option>October</option>
+                    <option>November</option>
+                    <option>December</option>
+                    </select>
+                </div>
+                <div className="options">Birthday Year
+                <select className="options">
+                <option value="2007">2007</option>
+                <option value="2006">2006</option>
+                <option value="2005">2005</option>
+                <option value="2004">2004</option>
+                <option value="2003">2003</option>
+                <option value="2002">2002</option>
+                <option value="2001">2001</option>
+                <option value="2000">2000</option>
+                <option value="1999">1999</option>
+                <option value="1998">1998</option>
+                <option value="1997">1997</option>
+                <option value="1996">1996</option>
+                <option value="1995">1995</option>
+                <option value="1994">1994</option>
+                <option value="1993">1993</option>
+                <option value="1992">1992</option>
+                <option value="1991">1991</option>
+                <option value="1990">1990</option>
+                <option value="1989">1989</option>
+                <option value="1988">1988</option>
+                <option value="1987">1987</option>
+                <option value="1986">1986</option>
+                <option value="1985">1985</option>
+                <option value="1984">1984</option>
+                <option value="1983">1983</option>
+                <option value="1982">1982</option>
+                <option value="1981">1981</option>
+                <option value="1980">1980</option>
+                <option value="1979">1979</option>
+                <option value="1978">1978</option>
+                <option value="1977">1977</option>
+                <option value="1976">1976</option>
+                <option value="1975">1975</option>
+                <option value="1974">1974</option>
+                <option value="1973">1973</option>
+                <option value="1972">1972</option>
+                <option value="1971">1971</option>
+                <option value="1970">1970</option>
+                <option value="1969">1969</option>
+                <option value="1968">1968</option>
+                <option value="1967">1967</option>
+                <option value="1966">1966</option>
+                <option value="1965">1965</option>
+                <option value="1964">1964</option>
+                <option value="1963">1963</option>
+                <option value="1962">1962</option>
+                <option value="1961">1961</option>
+                <option value="1960">1960</option>
+                <option value="1959">1959</option>
+                <option value="1958">1958</option>
+                <option value="1957">1957</option>
+                <option value="1956">1956</option>
+                <option value="1955">1955</option>
+                <option value="1954">1954</option>
+                <option value="1953">1953</option>
+                <option value="1952">1952</option>
+                <option value="1951">1951</option>
+                <option value="1950">1950</option>
+                <option value="1949">1949</option>
+                <option value="1948">1948</option>
+                <option value="1947">1947</option>
+                <option value="1946">1946</option>
+                <option value="1945">1945</option>
+                <option value="1944">1944</option>
+                <option value="1943">1943</option>
+                <option value="1942">1942</option>
+                <option value="1941">1941</option>
+                <option value="1940">1940</option>
+                <option value="1939">1939</option>
+                <option value="1938">1938</option>
+                <option value="1937">1937</option>
+                <option value="1936">1936</option>
+                <option value="1935">1935</option>
+                <option value="1934">1934</option>
+                <option value="1933">1933</option>
+                <option value="1932">1932</option>
+                <option value="1931">1931</option>
+                <option value="1930">1930</option>
+                <option value="1929">1929</option>
+                <option value="1928">1928</option>
+                <option value="1927">1927</option>
+                <option value="1926">1926</option>
+                <option value="1925">1925</option>
+                <option value="1924">1924</option>
+                <option value="1923">1923</option>
+                <option value="1922">1922</option>
+                <option value="1921">1921</option>
+                <option value="1920">1920</option>
+                <option value="1919">1919</option>
+                <option value="1918">1918</option>
+                <option value="1917">1917</option>
+                <option value="1916">1916</option>
+                <option value="1915">1915</option>
+                <option value="1914">1914</option>
+                <option value="1913">1913</option>
+                <option value="1912">1912</option>
+                <option value="1911">1911</option>
+                <option value="1910">1910</option>
+                <option value="1909">1909</option>
+                <option value="1908">1908</option>
+                <option value="1907">1907</option>
+                <option value="1906">1906</option>
+                <option value="1905">1905</option>
+                <option value="1904">1904</option>
+                <option value="1903">1903</option>
+                <option value="1902">1902</option>
+                <option value="1901">1901</option>
+                <option value="1900">1900</option>
+                    </select>
+                </div>
+            
+
+
+           </div>
            </div>
           
             </div> 
